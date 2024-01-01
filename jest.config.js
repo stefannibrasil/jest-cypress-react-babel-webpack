c69e8b2993630209c749961dbe52725a6535d4c6
Stefanni Brasil <stefannibrasil@gmail.com>
2024-01-01 11:16:53 -0700

    Add and configure jest-dom

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 const path = require('path')
-
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
   moduleNameMapper: {
     '\\.module\\.css': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
+  testEnvironment: 'jest-environment-jsdom',
 }
